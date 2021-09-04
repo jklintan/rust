b51c4223c37713bce45b68a57b41bce5d896b532
jklintan <joskl841@student.liu.se>
2021-09-04 17:28:03 +0200

    Add first functions to calculator class

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -5,6 +5,67 @@
 use native_windows_gui as nwg;
 use std::rc::Rc;
 
+const N_BUTTONS: usize = 16;
+
+struct Calculator {
+    var_x: f64,
+    var_y: f64,
+    ans: f64,
+    operator: String,
+    text_field: String,
+
+    width: i32,
+    height: i32,
+}
+
+impl Calculator {
+    // Static method, constructor
+    fn new(w: i32 , h: i32) -> Calculator {
+        Calculator {
+            var_x: 0.0, 
+            var_y: 0.0, 
+            ans: 0.0, 
+            operator: String::from(""),
+            text_field: String::from(""),
+            width: w, 
+            height: h
+        }
+    }
+
+    // Input detected
+    fn update_x(&mut self, x: f64) {
+        self.var_x = x;
+    }
+
+    // Input detected
+    fn update_y(&mut self, y: f64) {
+        self.var_y = y;
+    }
+
+    fn calculate(&mut self) {
+        if self.var_x != 0.0 && self.var_y != 0.0 {
+            if self.operator != "" {
+                if self.operator == "+" {
+                    self.ans = add(self.var_x, self.var_y);
+                } else if self.operator == "-" {
+                    self.ans = subtract(self.var_x, self.var_y);
+                }
+                // TODO: Add other operators
+            }
+        }
+    }
+
+
+}
+
+fn add(x: f64, y: f64) -> f64 {
+    return x + y;
+}
+
+fn subtract(x: f64, y: f64) -> f64 {
+    return x - y
+}
+
 fn create_button(text: &str, parent: &nwg::Window) -> nwg::Button{
     let mut btn = Default::default();
     nwg::Button::builder()
@@ -15,6 +76,25 @@ fn create_button(text: &str, parent: &nwg::Window) -> nwg::Button{
     btn
 }
 
+fn create_buttons(btn_texts: Vec<&str>, parent: &nwg::Window) -> [nwg::Button; N_BUTTONS]{
+    let mut buttons: [nwg::Button; N_BUTTONS] = Default::default();
+    let count = 0;
+    for label in btn_texts.iter(){
+        buttons[count] = create_button(label, parent);
+    };
+    buttons
+}
+
+fn create_textfield(text: &str, parent: &nwg::Window, readonly: bool) -> nwg::TextInput{
+    let mut textfield = Default::default();
+    nwg::TextInput::builder()
+        .text(text)
+        .parent(parent)
+        .readonly(readonly)
+        .build(&mut textfield);
+    textfield
+}
+
 fn main() {
     // Initialize the GUI
     nwg::init().expect("Failed to init Native Windows GUI");
@@ -22,24 +102,27 @@ fn main() {
 
     // Create window
     let mut window = Default::default();
-    let mut input = Default::default();
     let layout = Default::default();
+
+    // Create calculator
+    let calculator = Calculator {var_x: 0.0, var_y: 0.0, ans: 0.0, operator: String::from(""), text_field: String::from(""), width: 300, height: 400};
 
     // Create the GUI
     nwg::Window::builder()
-        .size((300, 400))
+        .size((calculator.width, calculator.height))
         .position((300, 300))
         .title("Calculator")
         .build(&mut window)
         .unwrap();
 
-    nwg::TextInput::builder()
-        .text("")
-        .parent(&window)
-        .readonly(true)
-        .build(&mut input);
+    // Create input field
+    let mut input_field = create_textfield("", &window, true);
 
     // Create buttons
+    let btn_texts = vec!["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "C", "=", "/"];
+
+    let buttons: [nwg::Button; N_BUTTONS] = create_buttons(btn_texts, &window);
+
     let mut one_btn = create_button("1", &window);
     let mut two_btn = create_button("2", &window);
     let mut three_btn = create_button("3", &window);
@@ -61,7 +144,7 @@ fn main() {
     nwg::GridLayout::builder()
         .parent(&window)
         .spacing(1)
-        .child_item(nwg::GridLayoutItem::new(&input, 0, 0, 4, 1))
+        .child_item(nwg::GridLayoutItem::new(&input_field, 0, 0, 4, 1))
         .child_item(nwg::GridLayoutItem::new(&one_btn, 0, 1, 1, 1))
         .child_item(nwg::GridLayoutItem::new(&two_btn, 1, 1, 1, 1))
         .child_item(nwg::GridLayoutItem::new(&three_btn, 2, 1, 1, 1))
@@ -94,7 +177,7 @@ fn main() {
                 },
             E::OnButtonClick => 
                 if &handle == &one_btn {
-                    nwg::modal_info_message(&events_window.handle, "Hello", &format!("Hello {}", input.text()));
+                    nwg::modal_info_message(&events_window.handle, "Hello", &format!("Hello {}", input_field.text()));
                 },
             _ => {}
         }
